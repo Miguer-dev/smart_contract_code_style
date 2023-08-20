47fd8f00f250a88a100c659e2491e307a917914a
Miguel Martinez <miguerdev@gmail.com>
2023-08-20 01:59:23 -0400

    Update DefaultDeployTemplate.s.sol

--- contract templates/foundry templates/DefaultDeployTemplate.s.sol
+++ contract templates/foundry templates/DefaultDeployTemplate.s.sol
@@ -14,3 +14,26 @@ contract DeployContract is Script {
         return mainContract;
     }
 }
+
+--------------------------------------------------------------------------------------
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Script} from "forge-std/Script.sol";
+import {MainContract} from "../src/MainContract.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
+
+contract DeployContract is Script {
+
+    function run() external returns (MainContract, HelperConfig) {
+        HelperConfig config = new HelperConfig();
+        (address parameter1, address parameter2, uint256 deployerKey) = config.activeNetworkConfig();       
+
+        vm.startBroadcast(deployerKey);
+        MainContract mainContract = new MainContract(parameter1,parameter2);       
+        vm.stopBroadcast();
+        return (mainContract,config);
+    }
+}
