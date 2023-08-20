6d984866b2294bff4d455f351c26d0d65c2724af
Miguel Martinez <miguerdev@gmail.com>
2023-08-19 20:57:59 -0400

    Update DefaultDeployContractTemplate.s.sol

--- DefaultDeployContractTemplate.s.sol
+++ DefaultDeployContractTemplate.s.sol
@@ -1,1 +1,16 @@
+// SPDX-License-Identifier: MIT
 
+pragma solidity >=0.8.0 <0.9.0;
+
+import {Script} from "forge-std/Script.sol";
+import {MainContract} from "../src/MainContract.sol";
+
+contract DeployContract is Script {
+    function run() external returns (BasicNft) {
+        vm.startBroadcast();
+        MainContract mainContract = new MainContract();
+        vm.stopBroadcast();
+
+        return mainContract;
+    }
+}
