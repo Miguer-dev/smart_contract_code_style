33a56aeb9cec659f7383dcccc957cf6a8f79a24d
Miguel Martinez <miguerdev@gmail.com>
2023-08-20 01:48:04 -0400

    Update DefaultHelperConfigTemplate.s.sol

--- contract templates/foundry templates/DefaultHelperConfigTemplate.s.sol
+++ contract templates/foundry templates/DefaultHelperConfigTemplate.s.sol
@@ -11,42 +11,61 @@ contract HelperConfig is Script {
     uint32 public constant CHAINID_GOERLI = 5;
     uint32 public constant CHAINID_SEPOLIA = 11155111;
     uint8 public constant CHAINID_ETHEREUM = 1;
+    uint16 public constant CHAINID_GANACHE = 5777;
 
     struct NetworkConfig {
         address adressNeeded1;
         address adressNeeded2;
+        uint256 deployerKey;
     }
 
     constructor() {
         if (block.chainid == CHAINID_GOERLI) activeNetworkConfig = getGoerliEthConfig();
         else if (block.chainid == CHAINID_SEPOLIA) activeNetworkConfig = getSepoliaEthConfig();
         else if (block.chainid == CHAINID_ETHEREUM) activeNetworkConfig = getMainEthConfig();
-        else activeNetworkConfig = getOrCreateTestChainEthConfig();
+        else if (block.chainid == CHAINID_GANACHE) activeNetworkConfig = getOrCreateGanacheChainEthConfig();
+        else activeNetworkConfig = getOrCreateAnvilChainEthConfig();
     }
 
     function getGoerliEthConfig() public pure returns (NetworkConfig memory) {
-        NetworkConfig memory goerliConfig = NetworkConfig(0x00000000000000000000000000000000000,0x00000000000000000000000000000000000);
+        uint256 deployerKey = vm.envUint("PRIVATE_KEY");
+        NetworkConfig memory goerliConfig = NetworkConfig(address(0),address(0),deployerKey);
         return goerliConfig;
     }
 
     function getSepoliaEthConfig() public pure returns (NetworkConfig memory) {
-        NetworkConfig memory sepoliaConfig = NetworkConfig(0x00000000000000000000000000000000000,0x00000000000000000000000000000000000);
+        uint256 deployerKey = vm.envUint("PRIVATE_KEY");
+        NetworkConfig memory sepoliaConfig = NetworkConfig(address(0),address(0),deployerKey);
         return sepoliaConfig;
     }
 
     function getMainEthConfig() public pure returns (NetworkConfig memory) {
-        NetworkConfig memory mainConfig = NetworkConfig(0x00000000000000000000000000000000000,0x00000000000000000000000000000000000);
+        uint256 deployerKey = vm.envUint("PRIVATE_KEY");
+        NetworkConfig memory mainConfig = NetworkConfig(address(0),address(0),deployerKey);
         return mainConfig;
     }
 
-    function getOrCreateTestChainEthConfig() public returns (NetworkConfig memory) {
-        if (activeNetworkConfig.priceFeed != address(0)) return activeNetworkConfig;
+    function getOrCreateGanacheChainEthConfig() public returns (NetworkConfig memory) {
+        if (activeNetworkConfig.adressNeeded1 != address(0)) return activeNetworkConfig;
+        uint256 deployerKey = vm.envUint("GANACHE_PRIVATE_KEY");        
 
         vm.startBroadcast();
         MockContract mockContract = new MockContract();
         vm.stopBroadcast();
 
-        NetworkConfig memory testConfig = NetworkConfig(address(mockContract));
+        NetworkConfig memory testConfig = NetworkConfig(address(mockContract),address(0),deployerKey);
+        return testConfig;
+    }
+
+    function getOrCreateAnvilChainEthConfig() public returns (NetworkConfig memory) {
+        if (activeNetworkConfig.adressNeeded1 != address(0)) return activeNetworkConfig;
+        uint256 deployerKey = vm.envUint("ANVIL_PRIVATE_KEY");        
+
+        vm.startBroadcast();
+        MockContract mockContract = new MockContract();
+        vm.stopBroadcast();
+
+        NetworkConfig memory testConfig = NetworkConfig(address(mockContract),address(0),deployerKey);
         return testConfig;
     }
 }
