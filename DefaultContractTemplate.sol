780cff97b067c2bb6cf69fb5abf391d4bbdb446b
Miguel Martinez <miguerdev@gmail.com>
2023-08-19 20:06:23 -0400

    Update DefaultContractTemplate.sol

--- DefaultContractTemplate.sol
+++ DefaultContractTemplate.sol
@@ -1,1 +1,72 @@
+// SPDX-License-Identifier: MIT
 
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// internal & private view & pure functions
+// external & public view & pure functions
+
+pragma solidity >=0.8.0 <0.9.0;
+
+/**
+ * @title DefaultContract
+ * @author Miguel Martinez
+ * @notice This contract is use as a template
+ * @dev developer notes
+ */
+contract DefaultContractTemplate {
+
+    ////////////////////
+    ///    Errors    ///
+    ////////////////////  
+
+    ///////////////////////
+    /// State Variables ///
+    ///////////////////////
+
+    ////////////////////
+    ///    Events    ///
+    ////////////////////   
+
+    ////////////////////
+    ///   Modifiers  ///
+    ////////////////////
+  
+    ////////////////////
+    ///   Functions  ///
+    ////////////////////
+    constructor( ) {}   	
+    
+    /////////////////////////////////////
+    /// External and Public Functions ///
+    /////////////////////////////////////
+   
+    //////////////////////////////////////
+    /// Internal and Private Functions ///
+    //////////////////////////////////////
+
+    ///////////////////////////////////////////
+    /// Internal and Private view Functions ///
+    ///////////////////////////////////////////
+
+    //////////////////////////////////////////
+    /// External and Public view Functions ///
+    //////////////////////////////////////////
+
+}
